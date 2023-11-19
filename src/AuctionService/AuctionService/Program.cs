0c449a8eb2e65aacfe6b858f61b438699fffcba3
Rahul Shastry D <rahul1999.jb@gmail.com>
2023-11-19 17:08:50 +0530

    Add project files.

---
+++ src/AuctionService/AuctionService/Program.cs
@@ -0,0 +1,19 @@
+using AuctionService.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddDbContext<AuctionDbContext>(opt =>
+{
+    opt.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+var app = builder.Build();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
